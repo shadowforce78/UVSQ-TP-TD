e47cbd8dc5dda924df2c841e82a7fa10ac2e2fa8
Adam Planque <22401313@me.uvsq.fr>
2024-10-23 14:51:05 +0200

    Refactor main.py to accurately calculate the number of red, blue, and green occurrences

--- dev/advent_of_code/2/main.py
+++ dev/advent_of_code/2/main.py
@@ -3,38 +3,35 @@ file = "testinput.txt"
 with open(file, "r") as f:
     data = f.readlines()
     lines = len(data)
-    
-totalRed = 0
-totalGreen = 0
-totalBlue = 0
-    
+
+
+red_count = 0
+blue_count = 0
+green_count = 0
+
 for i in range(lines):
     data[i] = data[i].replace("\n", "").replace("Game ", "").split(";")
     separated_data = []
     for item in data[i]:
         separated_data.extend(item.split(", "))
-        
+
     actualI = str(i + 1)
-        
+
     data[i] = [item.replace(actualI + ": ", "") for item in separated_data]
     data[i] = [item.split(" ") for item in data[i]]
-    
+
     for j in range(len(data[i])):
         if len(data[i][j]) > 2:
             data[i][j].pop(0)
         for k in range(len(data[i][j])):
-            if len(data[i][j][k]) > 2:
-                pass
-            else:
-                data[i][j][k] = int(data[i][j][k])
-            
-            if data[i][j][1] == "red":
-                totalRed += data[i][j][0]
-            elif data[i][j][1] == "green":
-                totalGreen += data[i][j][0]
-            elif data[i][j][1] == "blue":
-                totalBlue += data[i][j][0]
-                
-print("Red: " + str(totalRed))
-print("Green: " + str(totalGreen))
-print("Blue: " + str(totalBlue))
+            if data[i][j][k] == "red":
+                red_count += 1
+            elif data[i][j][k] == "blue":
+                blue_count += 1
+            elif data[i][j][k] == "green":
+                green_count += 1
+
+
+print(f"Number of 'red': {red_count}")
+print(f"Number of 'blue': {blue_count}")
+print(f"Number of 'green': {green_count}")
