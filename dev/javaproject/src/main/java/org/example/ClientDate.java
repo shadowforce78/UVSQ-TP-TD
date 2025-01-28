5d7c024375115f7d70422a82d4041912c19de16a
shadowforce78 <planque.adam@gmail.com>
2025-01-28 14:20:11 +0100

    Test tp1

---
+++ dev/javaproject/src/main/java/org/example/ClientDate.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ClientDate {
+    public static void main(String[] args) {
+        Date date = new Date(1, 1, 2021);
+        System.out.println(date);
+    }
+}
