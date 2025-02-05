90fdc0420aa1a24d7d80cc26cc0dd2bcda9b1434
shadowforce78 <planque.adam@gmail.com>
2025-02-05 09:57:27 +0100

    Comment out reservation print statements in ReservationClient for cleaner output

--- dev/java/javatp2/src/main/java/org/example/ReservationClient.java
+++ dev/java/javatp2/src/main/java/org/example/ReservationClient.java
@@ -4,10 +4,10 @@ public class ReservationClient {
     public static void main(String[] args) {
         PlageHoraire lPlage = new PlageHoraire(new Horaire(8, 30), new Horaire(9, 35));
         Reservation lReservation = new Reservation("Rendez-vous", lPlage, "2021-09-01");
-        System.out.println(lReservation);
+        // System.out.println(lReservation);
         PlageHoraire lPlage2 = new PlageHoraire(new Horaire(8, 30), new Horaire(9, 35));
         Reservation lReservation2 = new Reservation("Rendez-vous 2", lPlage2, "2021-09-01");
-        System.out.println(lReservation2);
+        // System.out.println(lReservation2);
         System.out.println(lReservation.compareTo(lReservation2));
     }
 }
