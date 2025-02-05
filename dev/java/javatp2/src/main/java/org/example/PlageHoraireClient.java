6e786943513ec60f6371463225995ca0d9c83b6c
shadowforce78 <planque.adam@gmail.com>
2025-02-05 09:46:32 +0100

    Update PlageHoraireClient to use a new time range for PlageHoraire

--- dev/java/javatp2/src/main/java/org/example/PlageHoraireClient.java
+++ dev/java/javatp2/src/main/java/org/example/PlageHoraireClient.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class PlageHoraireClient {
     public static void main(String[] args) {
-        PlageHoraire lPlage = new PlageHoraire(new Horaire(8, 30), new Horaire(8, 35));
+        PlageHoraire lPlage = new PlageHoraire(new Horaire(8, 30), new Horaire(10, 0));
         System.out.println(lPlage);
     }
 }
