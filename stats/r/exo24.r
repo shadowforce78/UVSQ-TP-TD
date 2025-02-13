7c57580a615d1f60a43cb75f57fce19d8d6ee091
shadowforce78 <planque.adam@gmail.com>
2025-02-13 08:23:42 +0100

    Add R scripts for CSV handling and number parity check; introduce ARM assembly example for variable manipulation

--- stats/r/exo24.r
+++ stats/r/exo24.r
@@ -1,4 +1,9 @@
-df=read.csv(file="filecsv.csv", header=T, sep=";")
-print("Dataframe:")
-print(df)
-print(is(df))
+# Un fichier CSV est un fichier qui contient un tableau (EX: Excel)
+# On peut écrire un CSV a la main avec des séparateurs (, ou ;) ou avec un logiciel de tableur
+
+
+df=read.csv(file="C:/Users/saumondeluxe/Desktop/DevProject/TP-TD/stats/r/filecsv.csv", header=T, sep=";")
+df
+is(df)
+
+# La structure de donnée ici est un dataframe list oldClass et vector
---
+++ stats/r/exo25.r
@@ -0,0 +1,7 @@
+pair=function(a){
+  if (a%%2==0) return("C'est un nombre pair")
+  else return("C'est un nombre impair")
+}
+
+pair(34)
+pair(21)
