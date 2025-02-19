54317ca068d866ae450bfe997ff7d670a733a2e8
shadowforce78 <planque.adam@gmail.com>
2025-02-19 13:55:55 +0100

    Add statistical functions and explanations for note analysis in exo27.R

---
+++ stats/r/exo27.R
@@ -0,0 +1,20 @@
+# Notes obtenues : 4 20 5 7 11 9 10 8 12 14 16 18 19 6
+note <- c(4, 20, 5, 7, 11, 9, 10, 8, 12, 14, 16, 18, 19, 6)
+# Tester : median() quantile() max() min() sort() summary() mean()
+median(note)
+quantile(note)
+max(note)
+min(note)
+sort(note)
+summary(note)
+mean(note)
+
+# A quoi sert chacune de ces fonctions ?
+# median() : renvoie la médiane d'un vecteur
+# quantile() : renvoie les quantiles d'un vecteur
+# max() : renvoie la valeur maximale d'un vecteur
+# min() : renvoie la valeur minimale d'un vecteur
+# sort() : renvoie un vecteur trié
+# summary() : renvoie un résumé des valeurs d'un vecteur
+# mean() : renvoie la moyenne d'un vecteur
+
