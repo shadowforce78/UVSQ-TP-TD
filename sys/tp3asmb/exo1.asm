0fa32dbe2a0cd10cc884ffa080f7ff28d57719be
shadowforce78 <planque.adam@gmail.com>
2025-02-19 10:21:59 +0100

    Refactor assembly code to load values from memory addresses for Big Endian / Little Endian exercise

--- sys/tp3asmb/exo1.asm
+++ sys/tp3asmb/exo1.asm
@@ -1,34 +1,17 @@
-; Exercice 1 Big Endian / Little Endian pour stocker des donn ees en m emoire
-
 SECTION INTVEC
 
 SECTION CODE
-    MOV #0x20000000, R0      ; Base address for data storage
 
-    ; Store 0x12345678 in Big Endian (12 34 56 78)
-    MOV #0x12, [R0]
-    MOV #0x34, [R0,#1]
-    MOV #0x56, [R0,#2]
-    MOV #0x78, [R0,#3]
+    ; Charger les adresses en mémoire avant d'accéder aux valeurs
+    LDR R3, var1           ; Charger l'adresse de n1 dans R3
+    LDR R1, [R3]         ; Charger la valeur de n1 dans R1
 
-    ; Store 0x12345678 in Little Endian (78 56 34 12)
-    MOV #0x78, [R0,#4]
-    MOV #0x56, [R0,#5]
-    MOV #0x34, [R0,#6]
-    MOV #0x12, [R0,#7]
-
-    ; Store 0xAABBCCDD in Big Endian (AA BB CC DD)
-    MOV #0xAA, [R0,#8]
-    MOV #0xBB, [R0,#9]
-    MOV #0xCC, [R0,#10]
-    MOV #0xDD, [R0,#11]
-
-    ; Store 0xAABBCCDD in Little Endian (DD CC BB AA)
-    MOV #0xDD, [R0,#12]
-    MOV #0xCC, [R0,#13]
-    MOV #0xBB, [R0,#14]
-    MOV #0xAA, [R0,#15]
+    LDR R3, var2           ; Charger l'adresse de n2 dans R3       
+    LDR R2, [R3]          ; Charger la valeur de n2 dans R2
 
     END
 
 SECTION DATA
+
+var1 ASSIGN32 0x12345678
+var2 ASSIGN32 0xAABBCCDD
