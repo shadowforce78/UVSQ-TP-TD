555bb7b168958283a6d232bf6daad23114367ead
shadowforce78 <planque.adam@gmail.com>
2025-03-10 16:09:35 +0100

    Add JUnit dependency and create initial test class for Calculatrice

--- dev/java/CalculatorJava/src/main/java/calculator/Calculatrice.java
+++ dev/java/CalculatorJava/src/main/java/calculator/Calculatrice.java
@@ -1,5 +1,6 @@
 package calculator;
 
+
 public class Calculatrice {
     public int ajouterUn(int a) {
         return a + 1;
---
+++ dev/java/CalculatorJava/src/test/java/calculator/CalculatriceTest.java
@@ -0,0 +1,18 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatriceTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ajouterUn() {
+    }
+}
