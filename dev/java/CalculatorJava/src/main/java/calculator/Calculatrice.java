c06d59017b8598e9be78debc439eea47ab420f6a
shadowforce78 <planque.adam@gmail.com>
2025-03-17 15:38:54 +0100

    Add soustraction method to Calculatrice for subtraction functionality

--- dev/java/CalculatorJava/src/main/java/calculator/Calculatrice.java
+++ dev/java/CalculatorJava/src/main/java/calculator/Calculatrice.java
@@ -13,4 +13,17 @@ public class Calculatrice {
         }
         return a;
     }
+
+    public static int soustraction(int a, int b) {
+        if (b >= 0) {
+            for (int i = 0; i < b; i++) {
+                a--;
+            }
+        } else {
+            for (int i = 0; i < Math.abs(b); i++) {
+                a++;
+            }
+        }
+        return a;
+    }
 }
