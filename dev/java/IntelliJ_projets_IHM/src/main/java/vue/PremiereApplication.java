36fbb6a638b0a1f23a66398b139732244de5360f
shadowforce78 <planque.adam@gmail.com>
2025-03-25 13:02:41 +0100

    Add CSS styling for PremiereApplication: define styles for root container and labels

--- dev/java/IntelliJ_projets_IHM/src/main/java/vue/PremiereApplication.java
+++ dev/java/IntelliJ_projets_IHM/src/main/java/vue/PremiereApplication.java
@@ -1,5 +1,7 @@
 package vue;
 
+import java.io.File;
+
 import javafx.application.Application;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -9,12 +11,24 @@ import javafx.scene.control.Label;
 public class PremiereApplication extends Application {
 
     public void start(Stage stage) {
+
+        File css = new File("css" + File.separator + "premierStyles.css");
+
         VBox root = new VBox(10);
+        root.getStyleClass().add("root-container");
+
         Label labelHello = new Label("Hello");
+        labelHello.getStyleClass().add("label-primary");
         root.getChildren().add(labelHello);
+
         Label labelHelloBis = new Label("Hello JavaFX");
+        labelHelloBis.getStyleClass().add("label-secondary");
         root.getChildren().add(labelHelloBis);
+
         Scene scene = new Scene(root, 300, 80);
+
+        scene.getStylesheets().add(css.toURI().toString());
+
         stage.setScene(scene);
         stage.setTitle("Hello JavaFX");
         stage.show();
