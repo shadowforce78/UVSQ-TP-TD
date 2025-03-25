a4fc298df9e15872ddba3252e3a7492144084343
shadowforce78 <planque.adam@gmail.com>
2025-03-25 12:58:27 +0100

    Enhance PremiereApplication: add VBox layout and labels for improved UI display

--- dev/java/IntelliJ_projets_IHM/src/main/java/vue/PremiereApplication.java
+++ dev/java/IntelliJ_projets_IHM/src/main/java/vue/PremiereApplication.java
@@ -1,14 +1,22 @@
 package vue;
+
 import javafx.application.Application;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 
 public class PremiereApplication extends Application {
 
-    public void start(Stage stage)   {
+    public void start(Stage stage) {
+        VBox root = new VBox(10);
+        Label labelHello = new Label("Hello");
+        root.getChildren().add(labelHello);
+        Label labelHelloBis = new Label("Hello JavaFX");
+        root.getChildren().add(labelHelloBis);
+        Scene scene = new Scene(root, 300, 80);
+        stage.setScene(scene);
         stage.setTitle("Hello JavaFX");
-        stage.setWidth(400);
-        stage.setHeight(150);
-        stage.centerOnScreen();
         stage.show();
     }
 
